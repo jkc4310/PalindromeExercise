d326713fbe33f60e24b288c6069888b64d12c616
kendall chapman <jkc4310@gmail.com>
2023-09-15 20:23:53 -0400

    palindrome exercise 1

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true )]
+        [InlineData("Howdy", false )]
+        public void MyTest(string word, bool expected)
         {
+            // arrange
+            var test = new WordSmith();
 
-        }
+            // act
+            bool  actual = test.IsAPalindrome(word);
+
+            // assert
+            Assert.Equal(expected, actual);
+        } 
+
+
     }
-}
+} 
